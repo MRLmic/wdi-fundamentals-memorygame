dd4277b4a64d29416f2e067820fb6c3a960bf5ae
Melissa Luna <Missi@Melissas-MacBook-Pro.local>
2017-08-11 12:25:01 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
-var cardOne = cards[3];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-var cardTwo = cards[1];
-console.log("User flipped " + cardTwo);
-cardsInPlay.push(cardTwo);
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {alert("You found a match!");}
-else {alert("sorry, try again");};
+var checkForMatch = function() {
+	if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) 
+		{alert("You found a match!");}
+	else if
+		(cardsInPlay.length === 2 && cardsInPlay[0] !== cardsInPlay[1]) {alert("Sorry, try again.");}
+}
+var flipCard = function(cardId) {
+	cardsInPlay.push(cards[cardId])	
+	console.log("User flipped " + cards[cardId])
+	checkForMatch();
+};
+flipCard(0);
+flipCard(1);
 
 
 
